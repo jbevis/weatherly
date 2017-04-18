f7139a4564241f0fb5bb9dbb304c5ed2918868b7
Jack Bevis <jack.bevis@gmail.com>
2017-04-18 08:24:53 -0600

    few more tests for weatherly app

--- test/Weatherly.test.js
+++ test/Weatherly.test.js
@@ -1,21 +1,43 @@
 import Weatherly from '../lib/components/Weatherly'
-// import Controls from '../lib/components/Controls'
 import React from 'react'
 import { shallow } from 'enzyme'
-// import StubData from './StubData'
-// import cleanCurrentData from '../lib/cleanCurrentData'
-// import cleanTenDayData from '../lib/cleanTenDayData'
-// import cleanHourlyData from '../lib/cleanHourlyData'
+import StubData from './StubData'
+import cleanCurrentData from '../lib/cleanCurrentData'
+import cleanTenDayData from '../lib/cleanTenDayData'
+import cleanHourlyData from '../lib/cleanHourlyData'
 
 describe('Weatherly: ', () => {
 
-  it.only('should be able to set state with correct data', () => {
+  it('should be able to set state with current data', () => {
     const wrapper = shallow(< Weatherly handleWeatherSearch = {() => {}}/>);
+    const currentData = cleanCurrentData(StubData)
 
     wrapper.setState({
-      current: 'Denver'
+      current: currentData
     })
 
-    expect(wrapper.state('current')).toEqual('Denver')
+    expect(wrapper.state('current').location).toEqual('Denver')
+  })
+
+  it('should be able to set state with hourly data', () => {
+    const wrapper = shallow(< Weatherly handleWeatherSearch = {() => {}}/>);
+    const hourlyData = cleanHourlyData(StubData)
+
+    wrapper.setState({
+      hourly: hourlyData
+    })
+
+    expect(wrapper.state('hourly').length).toEqual(7)
+  })
+
+  it('should be able to set state with ten day data', () => {
+    const wrapper = shallow(< Weatherly handleWeatherSearch = {() => {}}/>);
+    const tenDayData = cleanTenDayData(StubData)
+
+    wrapper.setState({
+      tenDay: tenDayData
+    })
+
+    expect(wrapper.state('tenDay').length).toEqual(10)
   })
 });
