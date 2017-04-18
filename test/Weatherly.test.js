c5a92e2bfac59c45f195f0665f6b0227904a0492
Jack Bevis <jack.bevis@gmail.com>
2017-04-17 19:00:21 -0600

    more testing features and cleaned up eslint a bit

--- test/Weatherly.test.js
+++ test/Weatherly.test.js
@@ -1,28 +1,21 @@
 import Weatherly from '../lib/components/Weatherly'
-import Controls from '../lib/components/Controls'
+// import Controls from '../lib/components/Controls'
 import React from 'react'
-import { shallow, mount } from 'enzyme'
-import StubData from './StubData'
-import cleanCurrentData from '../lib/cleanCurrentData'
-import cleanTenDayData from '../lib/cleanTenDayData'
-import cleanHourlyData from '../lib/cleanHourlyData'
+import { shallow } from 'enzyme'
+// import StubData from './StubData'
+// import cleanCurrentData from '../lib/cleanCurrentData'
+// import cleanTenDayData from '../lib/cleanTenDayData'
+// import cleanHourlyData from '../lib/cleanHourlyData'
 
 describe('Weatherly: ', () => {
 
-  it.skip('should be able to set state with correct data', () => {
-    const wrapper = shallow(< Weatherly />);
-    const controls = shallow(< Controls getWeather={() => {}} />);
-    const input = controls.find('#input-field');
-    const submitBtn = controls.find('#submitButton');
-    const hourlyTest = cleanHourlyData(StubData);
+  it.only('should be able to set state with correct data', () => {
+    const wrapper = shallow(< Weatherly handleWeatherSearch = {() => {}}/>);
 
-    expect(hourlyTest.length).toEqual(7)
-    // expect(wrapper.state('tenDay')).toEqual([])
+    wrapper.setState({
+      current: 'Denver'
+    })
 
-    input.simulate('change', { target: {value: 'Denver, CO' } })
-    submitBtn.simulate('click');
-
-    expect(wrapper.state('hourly').length).toEqual(7)
-    // expect(wrapper.state('tenDay').length).toEqual(10)
-  });
+    expect(wrapper.state('current')).toEqual('Denver')
+  })
 });
